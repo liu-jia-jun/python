beacc9c276b6ba1d2b99835a32843f757725d8ff
liu-jia-jun <3142467441@qq.com>
2021-10-30 00:32:18 +0800

    update python 2021.10.30

--- PythonNote/flask/app.py
+++ PythonNote/flask/app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+# flask 一个python框架，render_template python框架中的一个模块用于模板解析类似于jsp
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
+# 路由规则，路由不能重复，用户通过唯一路径访问特定的函数
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Python flask!'
+
+
+@app.route("/template")
+def testTemplate():
+    # 通过render_template 来返回给用户渲染后的网页文件
+    return render_template("index.html")
+
+@app.route("/user/<name>")
+def welcome(name):
+    return "你好，%s"%name
+
+# 指定传参类型
+@app.route("/user/<int:id>")
+def welcome2(id):
+    return "你好，%d"%id
+
+# 向页面传递一个变量
+@app.route("/test")
+def testTemplateAndParams():
+    name = ["张三","李四"]
+    task = {"任务":"打扫卫生","时间":"3小时"}
+    return render_template("index.html",name=name,task=task)
 
 if __name__ == '__main__':
     app.run()
